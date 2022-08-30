b2f96da4bb8ce890f14694507d78646252be3289
ahmed-abdulhuy <ahmelhuy@gmail.com>
2022-08-31 01:17:20 +0200

    Fix a bug: extra page for Resume file

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -35,7 +35,6 @@ function Resume() {
 
             <Document file={resumeLink}>
                 <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-                <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
             </Document>
 
             <div>
